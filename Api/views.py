8c77f9a95e039775876f182c5939ddaf0e97c78f
sarvesh821 <sarveshgoyal4152@gmail.com>
2024-07-23 14:17:51 +0530

    backend

--- Api/views.py
+++ Api/views.py
@@ -1,15 +1,18 @@
 from rest_framework import viewsets
 from Appraisal.models import Employee, Task, Attributes
 from .serializers import EmployeeSerializer, TaskSerializer, AttributesSerializer
-
+from rest_framework.permissions import IsAuthenticated
 class EmployeeViewSet(viewsets.ModelViewSet):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
+    permission_classes = [IsAuthenticated]
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
 
 class AttributesViewSet(viewsets.ModelViewSet):
     queryset = Attributes.objects.all()
     serializer_class = AttributesSerializer
+    permission_classes = [IsAuthenticated]
--- Appraisal/urls.py
+++ Appraisal/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('api/validate-token-admin/', views.validate_token_admin, name='validate_token'),
     path('api/validate-token-employee/', views.validate_token_employee, name='validate_token'),
     path('api/user-info/', views.user_info, name='user-info'),
